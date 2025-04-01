2a40d49fc264b4e3431b3e3846bdc44a341e7f52
Dominik Skerhak <dominik.skerhak@gmail.com>
2025-04-01 15:44:55 +0200

    Add rector rule to fix removed ParsedMailLogsRepository constants
    This rule replaces removed constants:
    - from `\Crm\PaymentsModule\Repositories\ParsedMailLogsRepository`
    - with `\Crm\PaymentsModule\Models\ParsedMailLog\ParsedMailLogStateEnum` enums.
    
    It is part of `CrmSetList::CRM_4_ENUMS` (which is part of bigger
    `CrmSetList::CRM_4` (use it to upgrade to CRM v4).
    
    remp/crm#3453

--- config/sets/crm-4-enums.php
+++ config/sets/crm-4-enums.php
@@ -63,4 +63,30 @@ return static function (RectorConfig $rectorConfig): void {
             )
         ]);
     }
+
+    // **********************************************************************
+    // Replace ParsedMailLogsRepository constants with ParsedMailLogStateEnum enums
+    $parsedMailLogsStates = [
+        'STATE_WITHOUT_VS' => 'WithoutVs',
+        'STATE_ALREADY_PAID' => 'AlreadyPaid',
+        'STATE_DUPLICATED_PAYMENT' => 'DuplicatedPayment',
+        'STATE_CHANGED_TO_PAID' => 'ChangedToPaid',
+        'STATE_PAYMENT_NOT_FOUND' => 'PaymentNotFound',
+        'STATE_DIFFERENT_AMOUNT' => 'DifferentAmount',
+        'STATE_AUTO_NEW_PAYMENT' => 'AutoNewPayment',
+        'STATE_NO_SIGN' => 'NoSign',
+        'STATE_NOT_VALID_SIGN' => 'NotValidSign',
+        'STATE_ALREADY_REFUNDED' => 'AlreadyRefunded',
+    ];
+
+    foreach ($parsedMailLogsStates as $oldPMLState => $newPMLState) {
+        $rectorConfig->ruleWithConfiguration(\Crm\Utils\Rector\UpgradeToCrm4\ReplaceConstWithEnumRector::class, [
+            new \Crm\Utils\Rector\RectorHelpers\ValueObject\ReplaceClassConstByClassEnumFetch(
+                'Crm\PaymentsModule\Repositories\ParsedMailLogsRepository',
+                $oldPMLState,
+                'Crm\PaymentsModule\Models\ParsedMailLog\ParsedMailLogStateEnum',
+                $newPMLState,
+            )
+        ]);
+    }
 };
